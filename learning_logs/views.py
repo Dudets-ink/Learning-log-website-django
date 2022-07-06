bef1e90940506fd5cf8687343cc1fe6c3af68981
Dima <pixel.art@bk.ru>
2022-07-06 21:08:17 +0300

    Added get_object_or_404 in views.py

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 from django.http import Http404
@@ -24,7 +24,7 @@ def topics(request):
 def topic(request, topic_id):
     """Выводит одну тему и все ее записи"""
 
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     check_topic_owner(topic, request)
 
     entries = topic.entry_set.order_by('-date_added')
